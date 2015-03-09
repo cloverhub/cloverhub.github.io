57207d693364888d3203f4256f436dfe27961b89
Shawn Clover <git@shawnclover.com>
2015-03-09 15:30:48 -0700

    Modify pizza scroll for loop for performance

--- views/js/main.js
+++ views/js/main.js
@@ -498,13 +498,24 @@ function logAverageFrame(times) {   // times is the array of User Timing measure
 // https://www.igvita.com/slides/2012/devtools-tips-and-tricks/jank-demo.html
 
 // Moves the sliding background pizzas based on scroll position
+
+// Store last Y position so it is not repeatedly calculated
+var lastY = 0;
+function onScroll() {
+  lastY = window.scrollY;
+}
+
+
 function updatePositions() {
   frame++;
   window.performance.mark("mark_start_frame");
 
   var items = document.querySelectorAll('.mover');
+  var scrollNumber = document.body.scrollTop / 1250; // Moved out of the for loop
+
+
   for (var i = 0; i < items.length; i++) {
-    var phase = Math.sin((document.body.scrollTop / 1250) + (i % 5));
+    var phase = Math.sin(scrollNumber + (i % 5));
     items[i].style.left = items[i].basicLeft + 100 * phase + 'px';
   }
 
